ab88cda685d286568f3688adc3fde298db7bc9b9
Daniel Pasillas <dpasi001@sierra-43.cs.ucr.edu>
2014-06-12 01:30:58 -0700

    Stop ogre from hogging mouse, keyboard

--- BaseApplication.cpp
+++ BaseApplication.cpp
@@ -94,10 +94,12 @@ void BaseApplication::createFrameListener(void)
     OIS::ParamList pl;
     size_t windowHnd = 0;
     std::ostringstream windowHndStr;
-
+    
     mWindow->getCustomAttribute("WINDOW", &windowHnd);
     windowHndStr << windowHnd;
     pl.insert(std::make_pair(std::string("WINDOW"), windowHndStr.str()));
+    pl.insert( std::make_pair(std::string("x11_mouse_grab"),std::string("false")) );
+    pl.insert( std::make_pair(std::string("x11_keyboard_grab"),std::string("false")) );
 
     mInputManager = OIS::InputManager::createInputSystem( pl );
 

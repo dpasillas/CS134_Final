8081741641c51c0fb8d7a5c4e8dad61cc954394e
Daniel Pasillas <dpasi001@sierra-27.cs.ucr.edu>
2014-06-12 21:45:03 -0700

    Collectible class

---
+++ collectible.cpp
@@ -0,0 +1,26 @@
+#include "collectible.h"
+#include <cstdlib>
+//#include <stdlib.h>
+
+using namespace Ogre;
+
+Collectible::Collectible(SceneManager* mSceneMgr) : Item(mSceneMgr) {
+
+    // Create an Entity
+    Entity* barrelCollectible = mSceneMgr->createEntity(getUniqueName("Collectible"), "Barrel.mesh");
+
+    // Create a SceneNode and attach the Entity to it
+    SceneNode* sceneNode = mSceneMgr->getRootSceneNode()->createChildSceneNode(getUniqueName("CollectibleNode"), pos);
+    
+    sceneNode->attachObject(collectibleEntity);
+    sceneNode->scale(1, 1, 2);
+    sceneNode->yaw(Degree(90));
+    
+    setNode(sceneNode);
+}
+
+//modify as you please :3
+void Collectible::update(Real dt) {
+    node->translate(Vector3(-25,0,0) * dt);
+    node->yaw(Degree(5));
+}
---
+++ collectible.h
@@ -0,0 +1,13 @@
+#ifdef COLLECTIBLE_H
+#define COLLECTIBLE_H
+
+#include "item.h"
+
+class Collectible : public Item {
+public:
+    class Collectible(Ogre::SceneManager* mSceneMgr);
+    
+    virtual void update(Ogre::Real);
+}
+
+#endif
